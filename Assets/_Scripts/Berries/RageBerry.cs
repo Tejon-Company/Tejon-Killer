9207fd686bd70a896779e435a53fc0b7c64032c0
Claudia Maciel Montero <c.maciel@udc.es>
2025-04-29 18:21:13 +0200

    Ajustado tiempo de rabia

--- Assets/_Scripts/Berries/RageBerry.cs
+++ Assets/_Scripts/Berries/RageBerry.cs
@@ -4,7 +4,7 @@ public class RageBerry : BerryEffect
 {
     [SerializeField] private float playerSpeedMultiplier = 2f;  
     [SerializeField] private float weaponFireRateMultiplier = 0.5f; 
-    [SerializeField] private float rageDuration = 30f;
+    [SerializeField] private float rageDuration = 7f;
 
 protected override void ApplyEffect(Collider player)
     {
