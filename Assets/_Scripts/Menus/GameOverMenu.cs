90610e0500f467a7a8f4c2f4911bbdc19366a697
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-05-12 17:25:46 +0200

    Añadir funcionalidad a los botones de la pantalla de Game Over

---
+++ Assets/_Scripts/Menus/GameOverMenu.cs
@@ -0,0 +1,25 @@
+﻿using _Scripts.Managers.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.Menus
+{
+    public class GameOverMenu : MonoBehaviour
+    {
+        private void Start()
+        {
+            MusicManager.Instance.PlayMusic(MusicManager.Instance.MenuBackgroundMusic);
+        }
+
+        public void GoToMainMenu()
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
+        }
+    }
+}
