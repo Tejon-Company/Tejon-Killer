b3524aae517bf89264987711aee63eb33235a3c9
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-05-14 21:24:42 +0200

    Solucionar errores

--- Assets/_Scripts/Hud/HeartsHUD.cs
+++ Assets/_Scripts/Hud/HeartsHUD.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 namespace _Scripts.Hud
 {
-    public class HeartsHUD : MonoBehaviour
+    public class HeartsHud : MonoBehaviour
     {
         [SerializeField]
         private Sprite heartSprite;
@@ -36,7 +36,6 @@ namespace _Scripts.Hud
             if (!EventManager.Current)
                 return;
 
-            EventManager.Current.healthChangedEvent.AddListener(UpdateHearts);
             EventManager.Current.damageCooldownEvent.AddListener(SetFlickerState);
             EventManager.Current.healthChangedEvent.AddListener(UpdateHearts);
             UpdateHearts();
@@ -48,7 +47,6 @@ namespace _Scripts.Hud
                 return;
 
             EventManager.Current.damageCooldownEvent.RemoveListener(SetFlickerState);
-            EventManager.Current.healthChangedEvent.RemoveListener(UpdateHearts);
             EventManager.Current.healthChangedEvent.RemoveListener(UpdateHearts);
         }
 
