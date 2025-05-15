e26b291bd7386c3e03f51382bd534bb113e89871
Carolina Rey López <114475444+CarolinaRey03@users.noreply.github.com>
2025-05-15 13:18:51 +0200

    TK-26 Añadir música a los niveles (#53)
    * Añadir música para los niveles y realizar transiciones de música al cambiar de escena
    
    * Añadir prefabs y arreglar música
    
    * Renombrar LevelMusic a SceneMusic y ajustar configuración de música en el menú principal
    
    ---------
    
    Co-authored-by: Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>

---
+++ Assets/_Scripts/Managers/Audio/SceneMusic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.Managers.Audio
+{
+    public class SceneMusic : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioClip music;
+
+        private void Start()
+        {
+            MusicManager.Instance.PlayMusic(music);
+        }
+    }
+}
